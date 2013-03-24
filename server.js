b44b93f6f3b6ed7a6a19ee979ba544641a601e2d
Orlando Del Aguila <orlando@freshout.us>
2013-03-23 23:50:41 -0600

    *

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ var app = express();
 var mongoose = require('mongoose');
 
 var allowCrossDomain = function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', "http://orlando.delagui.la http://localhost:4000");
+    res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
     next();
 };
